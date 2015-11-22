520d03b970f0f4a4b7709631fa5a74f1c318845e
miller <meossasa@unal.edu.co>
2015-11-22 09:08:44 -0500

    migraciones

---
+++ database/migrations/2015_10_17_162933_create_cavas_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCavasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cavas', function (Blueprint $table) {
+            $table->increments('codigo', 60);
+            $table->enum('tipo', ['Congelamiento', 'Maduración', 'Producto terminado']);
+            $table->decimal('temperatura_minima', 6, 3);
+            $table->decimal('temperatura_maxima', 6, 3);
+            $table->boolean('en_uso')->default(true);
+            $table->string('usuario_registrador', 60);
+
+            $table->timestamps();
+
+            $table->foreign('usuario_registrador')
+                ->references('correo')
+                ->on('usuarios')
+                ->onUpdate('cascade');  
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cavas');
+    }
+}
