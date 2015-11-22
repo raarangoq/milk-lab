2d1f94e6ce9a715925c561da80c7c6267b11e7b2
Ricardo Antonio Arango Quiroz <raarangoq@unal.edu.co>
2015-11-22 10:35:11 -0500

    productos derivados, tamanos

---
+++ app/Models/ProductoDerivado.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductoDerivado extends Model
+{
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'productos_derivados';
+
+    protected $primaryKey = 'nombre';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'nombre',
+        'tipo',
+        'descripcion_fisica',
+        'tiempo_de_vencimiento',
+        'instrucciones_de_la_etiqueta',
+        'temperatura_de_almacenamiento',
+        'unidad_de_medida',
+    ];
+}
---
+++ app/Models/Tamano.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tamano extends Model
+{
+     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tamanos';
+
+    protected $primaryKey = 'id';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+    	'id',
+        'cantidad',
+        'tipo',
+        'precio',
+        'producto_derivado',
+    ];
+}
