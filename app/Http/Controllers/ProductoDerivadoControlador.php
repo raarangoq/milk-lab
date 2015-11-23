74bdd71165b5fd3269efb1d3d4998f6295c2ea2c
miller <meossasa@unal.edu.co>
2015-11-23 15:18:31 -0500

    controladores producto derivado

--- app/Http/Controllers/ProductoDerivadoControlador.php
+++ app/Http/Controllers/ProductoDerivadoControlador.php
@@ -42,5 +42,64 @@ class ProductoDerivadoControlador extends Controller {
                 $productoDerivado->temperatura_de_almacenamiento = $request['temperatura_de_almacenamiento']; 
                 $productoDerivado->unidad_de_medida = $request['unidad_de_medida']; 
 
+
+                if($materiaPrima->save()){
+            
+            return redirect('listarMateriaPrima')->with('success','Materia prima registrada correctamente');
+        }
+
 	}
+
+	protected function getListarProductoDerivado(){
+
+		return view("ProductoDerivado/listarProductoDerivado");
+	}
+
+	protected function getEditarProductoDerivado(){
+ 
+       return view("ProductoDerivado/editarProductoDerivado");
+	}
+
+	protected function postEditarProductoDerivado(){
+
+		$this->validate($request,[
+
+		'nombre'=>'required',
+        'tipo'=>'required',
+        'descripcion_fisica'=>'required',
+        'tiempo_de_vencimiento'=>'required',
+        'instrucciones_de_la_etiqueta'=>'required',
+        'temperatura_de_almacenamiento'=>'required',
+        'unidad_de_medida'=>'required',
+
+			]);
+
+		  $nombreAnterior= $request['nombreAnterior'];
+          
+            $nuevonombre = $request['nombre'];
+               $nuevotipo = $request['tipo'];
+                $nuevadescripcion_fisica = $request['descripcion_fisica']; 
+                $nuevotiempo_de_vencimiento = $request['tiempo_de_vencimiento'];
+              $nuevainstrucciones_de_la_etiqueta = $request['instrucciones_de_la_etiqueta']; 
+             $nuevatemperatura_de_almacenamiento = $request['temperatura_de_almacenamiento'];$nuevaunidad_de_medida = $request['unidad_de_medida']; 
+
+                 if($productoDerivadoActualizada=ProductoDerivado::where('nombre',$nombreAnterior)
+                        ->update(['nombre'=>  $nuevoNombre,
+                                  'tipo'=>  $nuevoTipo,
+                                   'descripcion_fisica'=>$nuevadescripcion_fisica,
+        'tiempo_de_vencimiento'=>$nuevotiempo_de_vencimiento,
+        'instrucciones_de_la_etiqueta'=>$nuevainstrucciones_de_la_etiqueta,
+        'temperatura_de_almacenamiento'=>$nuevatemperatura_de_almacenamiento,
+        'unidad_de_medida'=>  $nuevoUnidad_de_medida,                               
+                          ])){
+
+ return redirect('listarProductoDerivado')->with('success','producto derivado editado correctamente');
+
+        }
+
+
+	}
+
+
+
 }
--- resources/views/ProductoDerivado/editarProductoDerivado.blade.php
+++ resources/views/ProductoDerivado/editarProductoDerivado.blade.php
@@ -2,6 +2,8 @@
 
 @section('contenido')
 
+      <!--   
+<input type="hidden" name="nombreAnterior" value={{$productoDerivado->nombre}}>
          <div class="wrapper">
           
             <div class="table">
@@ -177,6 +179,107 @@
 <!-- FIN Contenido -->
 
 @endSection
+-->
+
+
+
+@extends('layout.principal')
+
+@section('contenido')
+@include('alerts.request')
+
+<main>
+    <article>
+        <section>
+            <div id="stage" class="stage">
+                <form action="editarProductoDerivado" method="post" data-reactid=".0">
+                    <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>"> 
+                    <input type="hidden" name="nombreAnterior" value={{$productoDerivado->nombre}}>
+                    <fieldset data-reactid=".0.0">
+                        
+                        <legend data-reactid=".0.0.0">EDITAR PRODUCTO DERIVADO</legend>
+
+                          <div class="control" data-reactid=".0.0.3.0">
+                            <input name="nombre"  type="text" id="name" placeholder="nombre" required="" data-reactid=".0.0.3.0" class="active">
+                            <label data-reactid=".0.0.3.1">* Nombre </label>
+                        </div> 
+                          <div class="control" data-reactid=".0.0.1">
+
+                            <select name"tipo" data-reactid=".0.0.1.0" class="active">
+                             
+                               <option>Bebida</option>
+                              <option>Dulce</option>
+                              <option>Helado </option>
+                              <option>Queso </option>
+                           </select>
+                            
+                            <label data-reactid=".0.0.1.1">* Tipo </label>
+                         </div>
+                         <div class="control" data-reactid=".0.0.1">
+                           <textarea  name="descripcion_fisica" rows="10" data-reactid=".0.0.1.0" class="active"></textarea>
+                            <label data-reactid=".0.0.1.1">Descripcion fisica </label>
+                        </div>
+                         <div class="control" data-reactid=".0.0.3.0">
+                            <input name="tiempo_de_vencimiento"  type="number" id="name" placeholder="Descripcion fisica" required="" data-reactid=".0.0.3.0" class="active">
+                            <label data-reactid=".0.0.3.1">* Tiempo de vencimiento </label>
+                       </div>
+                        <div class="control" data-reactid=".0.0.3.0">
+                            <textarea name="instrucciones_de_la_etiqueta" rows="10"  data-reactid=".0.0.3.0" class="active">
+                            <label data-reactid=".0.0.3.1">Instrucciones de la etiqueta </label>
+                        </div>
+                          <div class="control" data-reactid=".0.0.1">
+
+                            <select name"temperatura_de_almacenamiento" data-reactid=".0.0.1.0" class="active">
+                             
+                               <option>Ambiente</option>
+                              <option>Refrigeración</option>
+                              <option>Congelación </option>
+                              
+                           </select>
+                            
+                            <label data-reactid=".0.0.1.1">*  Temperatura de almacenamiento  </label>
+                          </div>
+                         <div class="control" data-reactid=".0.0.1">
+
+                            <select name"unidad_de_medida" data-reactid=".0.0.1.0" class="active">
+                             
+                               <option>Litro</option>
+                              <option>Kilo</option>
+                           
+                           </select>
+                            
+                            <label data-reactid=".0.0.1.1">*  Unidad de medidad  </label>
+                        </div>
+
+                        
+
+                        
+
+
+
+
+
+                        <input type="submit" value="editar producto derivado" data-reactid=".0.0.6.2">
+
+
+                    </fieldset>
+
+                </form>
+            </div>
+
+        </section>
+
+    </article>
+
+</main>
+
+
+<!-- FIN Contenido -->
+
+@endSection
+
+
+
 
 
 
